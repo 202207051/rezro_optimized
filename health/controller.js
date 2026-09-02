622dc0671399b1fa893e9c4143a1641ee2db10d2
user1 <user1@localhost.localdomain>
2026-09-02 15:17:52 +0900

    add code

--- health/controller.js
+++
@@ -1,8 +0,0 @@
-import { sendSuccess } from "#utils/responseHelper.js";
-
-export function getHealth(req, res) {
-  return sendSuccess(res, {
-    status: "ok",
-    service: "rezero-backend",
-  });
-}
--- health/router.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-
-import { getHealth } from './controller.js';
-
-const router = express.Router();
-
-router.get('/', getHealth);
-
-export default router;
